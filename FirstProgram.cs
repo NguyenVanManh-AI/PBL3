801f105274c678687f2610072522b22faa1f35ff
SCOOUQ <quoctran07052002@gmail.com>
2022-03-07 10:23:00 +0700

    Quoc test

---
+++ FirstProgram.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PBL3
+{
+    public class FirstProgram
+    {
+        public static void Main(String [] agrs){
+            Console.WriteLine("Hello");
+        }
+    }
+}
