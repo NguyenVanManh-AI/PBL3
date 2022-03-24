0f751f6e985301e77be1a4baf260f937631302d1
Mạnh Nguyễn Văn <nguyenvanmanh2001it1@example.com>
2022-03-24 07:36:35 +0700

    Manh update

---
+++ QuanLyThuVien2/QuanLyThuVien2/capnhatnhanvien.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuanLyThuVien2
+{
+    public partial class updateInfor : Form
+    {
+        public updateInfor()
+        {
+            InitializeComponent();
+        }
+        Class.clsDatabase cls = new QuanLyThuVien2.Class.clsDatabase();
+        private void capnhatnhanvien_Load(object sender, EventArgs e)
+        {
+            cls.LoadData2DataGridView(dataGridView1, "select * from tblNhanVien where TAIKHOAN='" + Main.TenDN + "'");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (txtSoDienThoai.Text.Length - 1 <= 0)
+                MessageBox.Show("Số điện thoại không thể nhỏ hơn 0 số");
+            else
+                if (txtSoDienThoai.Text.Length - 1 > 12)
+                MessageBox.Show("Số điện thoại không thể lớn hơn 12 số");
+            else
+                    if (textTuoi.Text.Length - 1 <= 18 && textTuoi.Text.Length - 1 > 55)
+                MessageBox.Show("sai tuổi");
+            else
+            {
+                string strUpdate = "update tblNhanVien set TENNV='" + txtNHANVIEN.Text + "',DIACHI='" + txtDiaChi.Text + "',DIENTHOAI='" + txtSoDienThoai.Text + "',EMAIL='" + txtEmail.Text + "',ChucVu='" + textChhucVu.Text + "',Tuoi='" + textTuoi.Text + "' where TAIKHOAN='" + Main.TenDN + "'";
+                cls.ThucThiSQLTheoKetNoi(strUpdate);
+            }
+            cls.LoadData2DataGridView(dataGridView1, "select * from tblNhanVien where TAIKHOAN='" + Main.TenDN + "'");
+            MessageBox.Show("Sửa thành công");
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtNHANVIEN.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            txtDiaChi.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            txtSoDienThoai.Text = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+            txtEmail.Text = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
+            textChhucVu.Text = dataGridView1.Rows[e.RowIndex].Cells[7].Value.ToString();
+            textTuoi.Text = dataGridView1.Rows[e.RowIndex].Cells[8].Value.ToString();
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //string strDelete = "delete from tblNhanVien where TAIKHOAN='" + Main.TenDN + "'";
+            //cls.ThucThiSQLTheoPKN(strDelete);
+            //cls.LoadData2DataGridView(dataGridView1, "select * from tblNhanVien where TAIKHOAN='" + Main.TenDN + "'");
+            //MessageBox.Show("Xóa thành công");
+        }
+    }
+}
