25fb212d640118741bbfdcd4835b6e9c9c2f2298
Mạnh Nguyễn Văn <nguyenvanmanh2001it1@example.com>
2022-04-15 04:05:09 +0700

    Manh Update All Information

--- QuanLyThuVien2/QuanLyThuVien2/ForgotPassword.cs
+++ QuanLyThuVien2/QuanLyThuVien2/ForgotPassword.cs
@@ -29,7 +29,7 @@ namespace QuanLyThuVien2
         }
 
         SqlCommand sqlCommand;
-        string passwordText = "Ngocquoc0705@"; // 1 
+        string passwordText = "strongtech2908"; // 1 
         public Object layGiaTri(string sql) //lay gia tri cua  cot dau tien trong bang 
         {
             sqlCommand = new SqlCommand();
@@ -50,7 +50,7 @@ namespace QuanLyThuVien2
             cls.KetNoi();
         }
 
-        string fromText = "quocseoweb0705@gmail.com"; // 2 
+        string fromText = "strongtechmaster@gmail.com"; // 2 
        
          
 
@@ -122,7 +122,7 @@ namespace QuanLyThuVien2
             try
             {
                 Con = new SqlConnection();
-                Con.ConnectionString = @"Server =21AK22-COM\QUOC;" + "database=Library; Integrated Security = true"; // 3 
+                Con.ConnectionString = @"Server =DESKTOP-QCOSLTK\VANMANH;" + "database=Library2; Integrated Security = true"; // 3 
                 Con.Open();
             }
             catch {   }
--- QuanLyThuVien2/QuanLyThuVien2/UpdateInfor.cs
+++ QuanLyThuVien2/QuanLyThuVien2/UpdateInfor.cs
@@ -73,19 +73,11 @@ namespace QuanLyThuVien2
         }
         private void button5_Click(object sender, EventArgs e)
         {
-<<<<<<< HEAD
             if (txtSoDienThoai.Text.Length < 3)
                 MessageBox.Show("Phone number cannot be less than 3 digits");
             else
             {
                 if (txtSoDienThoai.Text.Length > 12)
-=======
-            if (txtSoDienThoai.Text.Length  < 3)
-                MessageBox.Show("Phone number cannot be less than 3 digits");
-            else
-            {
-                if (txtSoDienThoai.Text.Length  > 12)
->>>>>>> main
                     MessageBox.Show("Phone number cannot be more than 12 numbers");
                 else
                 {
@@ -93,32 +85,19 @@ namespace QuanLyThuVien2
                         MessageBox.Show("Wrong age");
                     else
                     {
-<<<<<<< HEAD
                         if (Checkso(textTuoi.Text))
                             MessageBox.Show("Invalid Age!");
                         else
                         {
                             if (CheckTen(txtNHANVIEN.Text))
-=======
-                        if(Checkso(textTuoi.Text)) 
-                            MessageBox.Show("Invalid Age!");
-                        else
-                        {
-                            if(CheckTen(txtNHANVIEN.Text)) 
->>>>>>> main
                                 MessageBox.Show("Invalid Name!");
                             else
                             {
                                 if (Checkso(txtSoDienThoai.Text))
                                     MessageBox.Show("Invalid Phone Number!");
-<<<<<<< HEAD
                                 else
                                 {
                                     if (!isValidEmail(txtEmail.Text) && !VerifyEmail(txtEmail.Text))
-=======
-                                else {
-                                    if (!isValidEmail(txtEmail.Text) && !VerifyEmail(txtEmail.Text)) 
->>>>>>> main
                                         MessageBox.Show("Invalid Email!");
                                     else
                                     {
